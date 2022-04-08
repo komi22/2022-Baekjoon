dfb96f05be4493718df37f731c18edf358600511
komi22 <92563389+komi22@users.noreply.github.com>
2022-04-08 23:03:47 +0900

    Create 1436.py

---
+++ Problem/Class 2/1436.py
@@ -0,0 +1,21 @@
+# 영화감독 슘 (1436)
+
+n = int(input())
+
+count = 0
+num = 0
+result = 0
+
+# 브루트포스 알고리즘이니, 단순하게 가보겠읍니다.
+while (1):
+    
+    if '666' in str(num):
+        result = num
+        count += 1
+    
+    if ( n == count ):
+        break
+
+    num += 1
+
+print(result)
